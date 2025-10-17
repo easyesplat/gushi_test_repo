82e99b75d1c7b48cb1ec5b7a7e680afcb5a715b4
Eric Choi <ericchoirr38@gmail.com>
2025-10-17 14:16:48 -0400

    test

--- probat/config.tsx
+++
@@ -1,3 +0,0 @@
-export const PROBAT_COMPONENTS: Record<string, { proposalId: string }> = {
-    "react-test-repo/src/components/Button.tsx": { proposalId: "14a7aa32-8999-4d80-a590-32d35bef8c4a" }
-};
